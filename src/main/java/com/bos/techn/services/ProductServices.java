ba91739140360cbb9847509aa11acfd44ec6d0b1
CGbrando98 <brandon.shew@capgemini.com>
2022-10-19 17:23:30 -0400

    Remove non-used pkgs

--- src/main/java/com/bos/techn/services/ProductServices.java
+++ src/main/java/com/bos/techn/services/ProductServices.java
@@ -1,10 +1,10 @@
 package com.bos.techn.services;
 
-import java.util.*;
+import java.util.*; 
 
 import com.bos.techn.beans.*; 
 import com.bos.techn.exceptions.*;
-import com.cg.onlineshop.exceptions.*;
+
 
 public interface ProductServices {
 	Product saveProduct(Product product) throws SavingDataException;
